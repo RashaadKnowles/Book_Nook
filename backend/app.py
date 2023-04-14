05b6ef2bb14a6754203a41f1219e2e7e79871f6d
unknown <rknowles12301@gmail.com>
2023-04-14 14:41:56 -0700

    Modified app.py file and added in UserfavoriteResource

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ from database.models import db
 from database.schemas import ma
 from resources.auth import LoginResource, RegisterResource
 from resources.cars import AllCarResource, UserCarResource
-from resources.books import UserReviewResource
+from resources.books import UserReviewResource , UserFavoriteResource
 from dotenv import load_dotenv
 from os import environ
 
@@ -57,6 +57,9 @@ def create_routes():
     api.add_resource(AllCarResource, '/api/cars')
     api.add_resource(UserCarResource, '/api/user_cars')
     # TODO: Create files for your Resources in resources folder, add them here
+    api.add_resource(UserFavoriteResource, '/api/favorite')
 
     api.add_resource(UserReviewResource, '/api/review')
     return api
+    
+    
