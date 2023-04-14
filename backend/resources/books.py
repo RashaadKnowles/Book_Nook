7148bef62e4cae8f842ff36d1a2e4cd2f4ddb7c2
unknown <rknowles12301@gmail.com>
2023-04-14 14:34:08 -0700

    Added in favroite_schema to my favorites resource.

--- backend/resources/books.py
+++ backend/resources/books.py
@@ -2,7 +2,7 @@ from flask import request
 from flask_jwt_extended import jwt_required, get_jwt_identity, verify_jwt_in_request
 from flask_restful import Resource
 from database.models import db, Review, Favorite
-from database.schemas import reviews_schema, rewiew_schema, favorite_schema, favorites_schema
+from database.schemas import rewiew_schema, favorite_schema 
 
 
 class UserReviewResource(Resource):
@@ -23,18 +23,18 @@ class UserFavoriteResource(Resource):
       def post(self):
             user_id = get_jwt_identity()
             form_data = request.get_json()
-            new_favorite = Favorite.load(form_data)
+            new_favorite = favorite_schema.load(form_data)
             new_favorite.user_id = user_id
             db.session.add(new_favorite)
             db.session.commit()
-            return Favorite.dump(new_favorite), 201
+            return favorite_schema.dump(new_favorite), 201
 
 
       @jwt_required()
       def get(self):
         user_id = get_jwt_identity()
         user_favorites = Favorite.query.filter_by(user_id=user_id)
-        return Favorite.dump(user_favorites), 200
+        return favorite_schema.dump(user_favorites), 200
         
         
 
